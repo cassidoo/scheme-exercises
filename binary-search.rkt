db1f8b10d2413a255c09e2efc3f9dbbcff0201d7
Cassidy Williams <cassidoo@gmail.com>
2014-08-12 23:33:30 -0400

    More binary search functions

--- binary-search.rkt
+++ binary-search.rkt
@@ -19,3 +19,71 @@
      )
    )
 )
+
+;============================
+; Adding the grammar
+; <tree> ::=  empty 
+;           | number <tree> <tree>
+;============================
+
+;creates an empty-tree
+(define (empty-tree)
+  '()
+)
+
+;returns #t or #f if the tree is empty or not
+(define (empty-tree? tree)
+   (if (equal? (empty-tree) tree)
+      #t
+      #f
+   )  
+)
+
+;creates a tree with only one node
+(define (bstree root)
+  (list root '() '()) ; Okay I know that procedural is all special, but look.  One line of code.  Beautiful.
+)
+
+;returns the root of the given tree
+(define (tree->root tree)
+  (car tree)  
+)
+
+;returns the left subtree
+(define (tree->left tree)
+  (car (cdr tree))
+)
+
+;returns the right subtree
+(define (tree->right tree)
+  (car (cdr (cdr tree)))
+)
+
+;inserts a node with value n into the tree while still maintaining
+;the binary tree's properties mentioned above.
+(define (insert-tree n t)
+  (cond 
+    ((empty-tree? t) (bstree n))
+    ((> n (tree->root t)) (list (car t) (car (cdr t)) (insert-tree n (car (cdr (cdr t))))))
+    (else (list (car t) (insert-tree n (car (cdr t))) (car (cdr (cdr t)))))
+  )  
+)
+
+;return a list containing all the values of the tree in sorted order
+(define (tree->list tree)
+  (append
+    (append
+      (treetolist (cdr (tree->left tree)) (list (car (tree->left tree))))
+      (list(tree->root tree)))
+    (treetolist (cdr (tree->right tree)) (list (car (tree->right tree)))))
+)
+
+;helper method
+(define (treetolist t f)
+  (cond 
+    ((null? t) f)
+    ((null? (car t)) (treetolist (cdr t) f))
+    (else (append (list (car t) f)))
+  )
+)
+
